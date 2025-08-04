65e93f66690d6909527b35aaa66aa4c89d586512
Sergei Ponomarev <mdhr@ya.ru>
2025-08-04 10:56:16 +0000

    0.7 lesson 9 final

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Birthday
+from .validators import real_age
+
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+
+# class BirthdayForm(forms.Form):
+#     first_name = forms.CharField(label='Имя', max_length=20)
+#     last_name = forms.CharField(
+#         label='Фамилия', required=False, help_text='Необязательное поле'
+#     )
+#     birthday = forms.DateField(
+#         label='Дата рождения',
+#         widget=forms.DateInput(attrs={'type': 'date'}),
+#         validators=(real_age,),
+#     )
 
 
 class BirthdayForm(forms.ModelForm):
@@ -14,3 +31,21 @@ class BirthdayForm(forms.ModelForm):
                 format='%Y-%m-%d'
             )
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
