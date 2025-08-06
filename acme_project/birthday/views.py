a5e83dd90fa0a365da674e970bfe4cb9d777b13c
Sergei Ponomarev <mdhr@ya.ru>
2025-08-06 14:26:34 +0000

    1.0 lesson 1 final

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
@@ -25,9 +26,12 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context) 
 
 
 def delete_birthday(request, pk):
